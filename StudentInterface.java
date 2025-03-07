ab25502b69c1a4c2de34adfb8d31739d73084277
ErinSwallow <74028614+ErinSwallow@users.noreply.github.com>
2025-03-07 14:55:39 -0500

    Create StudentInterface.java
    Interface for the student class

---
+++ StudentInterface.java
@@ -0,0 +1,7 @@
+public interface StudentInterface{
+	public void ViewCourses(Course[]courses);
+	public void ViewOpenCourses(Course[]courses);
+	public void RegisterInCourse(Course[]courses);
+	public void WithdrawFromCourse(Course[]courses);
+	public void ViewRegisteredCourses(Course[]courses);
+}
